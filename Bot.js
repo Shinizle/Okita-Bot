922a3ebe3ee94f24e52ad81a93d238cac80c34cb
Irvan Kharisma Fihan <blackparade76@ymail.com>
2018-02-25 20:21:11 +0700

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === '>Hi') {
+    if (message.content === 'ping') {
     	message.reply('iya sayang');
   	}
 });
