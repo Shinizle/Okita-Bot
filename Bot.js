99e55de8c221ae7eba8dd2c58e47412b21fd8276
Irvan Kharisma Fihan <blackparade76@ymail.com>
2018-03-02 18:24:27 +0700

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,8 +1,16 @@
+// Import the discord.js module
 const Discord = require('discord.js');
+
+// Create an instance of a Discord client
 const client = new Discord.Client();
 
+// The token of your bot - https://discordapp.com/developers/applications/me
+const token = 'NDE3Mjk0MjgzMDkyMTMxODQw.DXrFcA.d9iEtK-bJ_5HBjuYQug487YSJ9w';
+
+// The ready event is vital, it means that your bot will only start reacting to information
+// from Discord _after_ ready is emitted
 client.on('ready', () => {
-    console.log('I am ready!');
+  console.log('I am ready!');
 });
 
 client.on('message', message => {
@@ -15,6 +23,14 @@ client.on('message', message => {
     else if (message.content === ">addrole Fire") {
     	member.addRole(Fire).catch(console.error);
   	}
+});
+
+client.on('message', message => {
+  // If the message is "what is my avatar"
+  if (message.content === '>avatar') {
+    // Send the user's avatar URL
+    message.reply(message.author.avatarURL);
+  }
 });
 
 // THIS  MUST  BE  THIS  WAY
