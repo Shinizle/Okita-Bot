37324cea95a6e66011b72bd2700955207594ae10
Irvan Kharisma Fihan <blackparade76@ymail.com>
2018-09-03 13:13:43 +0700

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -36,6 +36,33 @@ client.on('message', message => {
     if (message.content === "!hello") {
         message.channel.send("", { files: ["https://cdn.discordapp.com/attachments/483868862987894799/484506814218764296/Stamp45.png"]});
     }
+    if (message.content === "!otsu") {
+        message.channel.send("", { files: ["https://cdn.discordapp.com/attachments/484198935930339354/486053252706729994/Stamp10.png"]});
+    }
+    if (message.content === "!ok") {
+        message.channel.send("", { files: ["https://cdn.discordapp.com/attachments/484198935930339354/486053459871793164/Stamp44.png"]});
+    }
+    if (message.content === "!dokkan") {
+        message.channel.send("", { files: ["https://cdn.discordapp.com/attachments/484198935930339354/486054035040894989/Stamp148.png"]});
+    }
+    if (message.content === "!teehe") {
+        message.channel.send("", { files: ["https://cdn.discordapp.com/attachments/484198935930339354/486054073913573376/Stamp142.png"]});
+    }
+    if (message.content === "!letsgo") {
+        message.channel.send("", { files: ["https://cdn.discordapp.com/attachments/484198935930339354/486054103047471104/Stamp75.png"]});
+    }
+    if (message.content === "!aye") {
+        message.channel.send("", { files: ["https://gbf.wiki/images/d/d2/Stamp147.png"]});
+    }
+    if (message.content === "!notgood") {
+        message.channel.send("", { files: ["https://gbf.wiki/images/5/52/Stamp146.png"]});
+    }
+    if (message.content === "!anoo") {
+        message.channel.send("", { files: ["https://gbf.wiki/images/d/dd/Stamp64.png"]});
+    }
+    //if (message.content === "!otsu") {
+        //message.channel.send("", { files: [""]});
+    //}
 });
 
 // Log our bot in using the token from https://discordapp.com/developers/applications/me
